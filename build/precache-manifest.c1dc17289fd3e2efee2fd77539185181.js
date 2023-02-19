0e444a04843a70dc0828a88115fa367e08b6914f
ar2745 <76749119+ar2745@users.noreply.github.com>
2023-02-19 18:54:13 -0500

    Add files via upload

---
+++ build/precache-manifest.c1dc17289fd3e2efee2fd77539185181.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "25608e0a8a248f7d8867",
+    "url": "/static/css/main.dfdc421d.chunk.css"
+  },
+  {
+    "revision": "25608e0a8a248f7d8867",
+    "url": "/static/js/main.25608e0a.chunk.js"
+  },
+  {
+    "revision": "046f1ed2190a6d64c03b",
+    "url": "/static/css/1.666a1553.chunk.css"
+  },
+  {
+    "revision": "046f1ed2190a6d64c03b",
+    "url": "/static/js/1.046f1ed2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "da70255120845f3066c5bdcc7367f259",
+    "url": "/static/media/tether.da702551.png"
+  },
+  {
+    "revision": "483a7ac253296f85886e996115ad1e14",
+    "url": "/index.html"
+  }
+];
